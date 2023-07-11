fbd705e944e8600e9e015da4e55ef5e3e03c9481
Jose Paulino <j.paulino1@hotmail.com>
2023-07-11 20:59:30 -0300

    armazenar funcao em uma variave, Fc arrow, Fc implicito

--- funcao2.js
+++ funcao2.js
@@ -1,5 +1,16 @@
-//Armazenando uma funcção em uma variavel
+// Armazenando uma função em uma variavel
 const imprimirSoma = function (a, b){//função anonima
   console.log(a + b)
 } 
 imprimirSoma(2, 3)
+
+// Armazenando uma função arrow em uma variavel
+const soma = (a, b) => { //Não utiliza o nome function
+return a+ b
+}
+console.log(soma(2, 3))
+
+// Arrow function Retorno implícito
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(20, 5))
