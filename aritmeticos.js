0ed4bd36265f55cea97098eb40df1946e07489eb
José Paulino <j.paulino1@hotmail.com>
2023-07-19 21:55:15 -0300

    Update aritmeticos.js

--- aritmeticos.js
+++ aritmeticos.js
@@ -1,12 +1,12 @@
 //Forma reduzida para criar 4 variavel (usando destructing)
-const [a, b, c, d] = [3, 5, 1, 15] 
+const [a, b, c, d, e] = [3, 5, 1, 15, 20] 
 
-const soma = a + b + c + d 
+const soma = a + b + c + d + e
 const subtracao = d - b
 const multiplicacao = a * b
 const divisao = d / a
 const modulo = a % 2
 
-console.log(soma)
+console.log(soma) * 2
 console.log(subtracao)
 console.log(soma, subtracao, multiplicacao, divisao, modulo)
